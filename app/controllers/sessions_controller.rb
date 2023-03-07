e961f2a20e4d2a34b48c3742bdb63559c91cd0b1
jfsoza <juan.soza@kellogg.northwestern.edu>
2023-03-07 04:53:53 +0000

    final changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,8 @@ class SessionsController < ApplicationController
             #if they do check password
             #if params["password"] ==@user["password"]
             if BCrypt::Password.new(@user["password"]) ==params["password"]
+                cookies["monster"]= "me like cookies"
+                session["user_id"]=@user["id"]
             #if they do login in
             redirect_to "/companies"
             else
@@ -20,6 +22,11 @@ class SessionsController < ApplicationController
         
       
         
+    end
+
+    def destroy
+        session["user_id"] = nil
+        redirect_to "/sessions/new"
     end
 
 end
